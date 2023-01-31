6c9861a679f5b7c88c9830585cbdb329b0ff4597
József Nyitrai <nyitrai.joco@outlook.com>
2023-01-31 18:13:15 +0100

    Remove MemoryStream variable.

--- CallLogger/Program.cs
+++ CallLogger/Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-            var x = new MemoryStream();
             Logger logger = new(Logger.LoggerTypeEnum.console);
             logger.CreateLog(Logger.LogLevelEnum.debug, "debug logger");
             logger.CreateLog(Logger.LogLevelEnum.info, "info logger");
